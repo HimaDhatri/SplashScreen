51fe527ae41eed0eeb78034382447e327bdf3b80
subramanyayash <subramanya.star@gmail.com>
2015-06-10 19:36:03 +0530

    test

--- SplashScreen/www/js/index.js
+++ SplashScreen/www/js/index.js
@@ -54,6 +54,20 @@ var app = {
 
 };
 function onBackKeyDown() {
-    alert("exit");
-    navigator.app.exitApp();
+    
+    navigator.notification.confirm(
+            'Do you want to exit!', // message
+             onConfirm,            // callback to invoke with index of button pressed
+            'AsseTrack',           // title
+            ['Yes', 'No']         // buttonLabels
+        );
+}
+   
+
+
+function onConfirm(buttonIndex) {
+    //alert('You selected button ' + buttonIndex);
+    if (buttonIndex == 1) {
+        navigator.app.exitApp();
+    }
 }
